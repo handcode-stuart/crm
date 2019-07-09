b3a47526ff2541611d07ff3c3bde497c62cbbebb
Stuart Morris <admin@handcode.co.uk>
2019-07-09 18:13:37 +0100

    Style tweak

--- client/src/components/AuthSidebar.js
+++ client/src/components/AuthSidebar.js
@@ -7,26 +7,24 @@ import styled from "styled-components";
 
 const StyledAuthSidebar = styled.nav`
     position: absolute;
-    padding: 85px 0 20px;
+    padding: 85px 30px 20px;
     top: 0;
     left: 0;
-    width: 65px;
+    width: 160px;
     height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
-    align-items: center;
     border-right: 1px solid ${props => props.theme.colorGreyLight};
 
     ul {
         list-style-type: none;
-        margin: 0;
+        margin: 20px 0 0;
         padding: 0;
-        text-align: center;
 
         &:first-child {
             li {
-                margin: 14px 0;
+                margin-bottom: 20px;
             }
         }
 
@@ -39,11 +37,11 @@ const StyledAuthSidebar = styled.nav`
 
         li {
             display: block;
-            font-size: 20px;
+            font-size: 14px;
 
             a {
                 margin: 0;
-                color: ${props => props.theme.secondaryColor};
+                color: #000;
                 text-decoration: none;
 
                 &:hover {
@@ -59,23 +57,23 @@ const AuthSidebar = ({ logout }) => {
         <StyledAuthSidebar>
             <ul>
                 <li>
-                    <Link to='/'>🏠</Link>
+                    <Link to='/'>Dashboard</Link>
                 </li>
                 <li>
-                    <Link to='/jobs'>💼</Link>
+                    <Link to='/jobs'>Jobs</Link>
                 </li>
                 <li>
-                    <Link to='/contacts'>👩🏻‍💼</Link>
+                    <Link to='/contacts'>Contacts</Link>
                 </li>
                 <li>
-                    <Link to='/companies'>🏦</Link>
+                    <Link to='/companies'>Companies</Link>
                 </li>
                 <li>
-                    <Link to='/settings'>⚙️</Link>
+                    <Link to='/settings'>Settings</Link>
                 </li>
             </ul>
             <ul>
-                <li onClick={logout}>🚪</li>
+                <li onClick={logout}>Logout</li>
             </ul>
         </StyledAuthSidebar>
     );
--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledPage = styled.div`
-    padding: 85px 40px 20px 105px;
+    padding: 85px 40px 20px calc(160px + 40px);
 `;
 
 const Page = ({ children }) => {
