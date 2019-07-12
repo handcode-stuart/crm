340b50108ac8b306d27e3c904e721d926b3437ba
Stuart Morris <admin@handcode.co.uk>
2019-07-12 19:02:45 +0100

    Added render and snapshot tests for Spinner

--- client/src/components/__tests__/Spinner.test.js
+++ client/src/components/__tests__/Spinner.test.js
@@ -3,8 +3,17 @@ import { shallow } from "enzyme";
 import Spinner from "../Spinner";
 
 describe("<Spinner />", () => {
-    it("should render", () => {
+    it("renders", () => {
+        shallow(<Spinner />);
+    });
+
+    it("renders and matches the snapshot", () => {
         const wrapper = shallow(<Spinner />);
-        expect(wrapper.html()).toBe("<div><p>Loading...</p></div>");
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    it("renders with fullpage prop and matches the snapshot", () => {
+        const wrapper = shallow(<Spinner fullpage />);
+        expect(wrapper.html()).toMatchSnapshot();
     });
 });
